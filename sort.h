3e0d4d8a0e7019af2cae1e685119823512a993a1
Ravan Asad <ravanasad@outlook.com>
2024-01-18 13:14:49 -0800

    Created 1-insertion_sort_list.c

--- sort.h
+++ sort.h
@@ -2,7 +2,8 @@
 #define SORT_H
 
 #include <stddef.h>
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -22,7 +23,7 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
-
+void insertion_sort_list(listint_t **list);
 #endif
 
 
